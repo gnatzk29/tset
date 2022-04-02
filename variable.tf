da658659961ff95a2cd24fd21adabf9bdc737534
gnatzk29 <70805945+gnatzk29@users.noreply.github.com>
2022-04-02 13:53:45 +0300

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "access_key" {
+    default = "AKIAUD4AUSSJEGBCF54Y"
+  
+}
+variable "secret_key" {
+    default = "QWkoxFTKt9JOnLY/Mvbb1HXw/ib9UvmP5qLA8e2l"
+  
+}
+variable "instance_type" {
+    default = "t2.micro"
+  
+}
+variable "public_subnet_cidrs" {
+    default = [
+        "10.0.1.0/24",
+        "10.0.2.0/24"
+    ]
+  
+}
